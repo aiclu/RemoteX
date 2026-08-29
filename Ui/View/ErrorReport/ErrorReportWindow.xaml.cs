97cffbd7bc0ef5bffaeeff0f6838f35f81caa609
Xiangyu <Xiangyu.Li@budweiserapac.com>
2026-08-29 23:44:30 +0800

    chore: update project issue links

--- Ui/View/ErrorReport/ErrorReportWindow.xaml.cs
+++ Ui/View/ErrorReport/ErrorReportWindow.xaml.cs
@@ -171,7 +171,7 @@ namespace _1RM.View.ErrorReport
         {
             try
             {
-                HyperlinkHelper.OpenUriBySystem("https://github.com/1Remote/1Remote/issues");
+                HyperlinkHelper.OpenUriBySystem("https://github.com/aiclu/RemoteX/issues");
             }
             catch
             {
